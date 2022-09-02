7e996a2f624b2bbafe0badff9dab0cb5e4437ad8
route-2 <ruthurao@gmail.com>
2022-09-02 23:34:41 +0530

    connect/ipfs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import GetAccount from './hooks/GetAccount';
+import GetContract from './hooks/GetContract';
+import { ImageUpload } from 'react-ipfs-uploader'
+import { useState } from 'react';
+import { FileUpload } from 'react-ipfs-uploader'
 
 function App() {
+  const account = GetAccount()
+  const contract = GetContract('0x5FbDB2315678afecb367f032d93F642f64180aa3', [])
+  const [imageUrl, setImageUrl] = useState('')
+  const [fileUrl, setFileUrl] = useState('')
+ 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+
+   
+    <div className="text-3xl font-bold underline">
+    <label> {account} </label>
+   
+
+    <ConnectButton />;
+    </div>
+    <div>
+            <ImageUpload setUrl={setImageUrl} />
+            ImageUrl : <a
+                href={imageUrl}
+                target='_blank'
+                rel='noopener noreferrer'
+            >
+                {imageUrl}
+            </a>
+        </div>
+
+
+        <div>
+            <FileUpload setUrl={setFileUrl} />
+            FileUrl : <a
+                href={fileUrl}
+                target='_blank'
+                rel='noopener noreferrer'
+            >
+                {fileUrl}
+            </a>
+        </div>
+    </>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,48 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum, chain.ropsten, chain.rinkeby, chain.polygonMumbai],
+  [
+    alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+<WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
   </React.StrictMode>
 );
 
