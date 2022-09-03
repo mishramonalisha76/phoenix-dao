ce9098795550d273200e7ace06b86f681ae41cb8
Monalisha Mishra <mishramonalisha76@gmail.com>
2022-09-03 17:25:48 +0530

    added logo

--- src/components/LaunchPage/launchPage.js
+++ src/components/LaunchPage/launchPage.js
@@ -11,7 +11,7 @@ export default function LaunchPage() {
   return (
 
     <div class="flex">
-      <div class="flex flex-col flex-initial w-3/5 m-8">
+      <div class="flex flex-col flex-initial w-3/5 m-8 ml-14 ">
         <div class="flex flex-row  mb-44">
           <span>
             <img src={logo} alt='logo' class="w-16 h-16 mr-6 -mt-4 rounded-full" />
@@ -33,7 +33,7 @@ export default function LaunchPage() {
           </div>
         </div>
       </div>
-      <div class="flex-initial w-2/5">
+      <div class="flex-initial w-3/6 h-screen">
         <img class="w-full h-full" src={Lights} alt="lights" />
       </div>
     </div>
